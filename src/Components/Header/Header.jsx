c5eec9d335ca34b3e73e52153e5657366f61b81c
Rbrahma1245 <rbrahma1245@gmail.com>
2024-02-16 16:10:40 +0530

    fix header css

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,16 +5,16 @@ import logo from "../../Images/logo.jpg";
 import { NavLink } from "react-router-dom";
 
 export class Header extends Component {
-  constructor() {
-    super();
-  }
-
   render() {
     return (
       <div className="header-container" style={{ display: "flex" }}>
         <div className="header-left">
           <img src={logo} alt="logo" />
-          <NavLink to="/" className="link">
+          <NavLink
+            to="/"
+            className="link"
+            onClick={() => this.handleNavLinkClick("/")}
+          >
             HOME
           </NavLink>
           <NavLink to="/product" className="link">
